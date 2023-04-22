051bed32d433c76429e7079501bc5045f62bda35
Rahul Chauhan <r.rahul.c@gmail.com>
2023-04-22 20:39:12 +0530

    updates

--- TradeTestNetBinance.py
+++ TradeTestNetBinance.py
@@ -44,57 +44,63 @@ if __name__ == "__main__":
     # Initiate Client
     umFuturesClient = initiateClient(base_url,key,secret)
 
-    # Set Params to Buy
-    params = {
-    'symbol': 'BTCUSDT',
-    'side': 'BUY',
-    'type': 'LIMIT',
-    'timeInForce': 'GTC',
-    'quantity': 0.003,
-    'price': round(float(umFuturesClient.ticker_price("BTCUSDT")["price"]) + 10) # Setting price as ticker price plus 10 so that buy order gets filled
-    }
-
-    # Execute Buy
-    buyOrderId = executeOrder(params)
-    print('\n\n*** BUY Order Sent ****')
-    print(f'Buy Order Id is {buyOrderId}')
-
-    # Wait for 30 seconds
-    time.sleep(30)
-
-    # Query Order to check if it was FILLED
-    print("\n\n*** Checking Buy Order Status ***")
-    orderStatus = queryOrder("BTCUSDT",buyOrderId)
-    print(f'*** Order status for order id {buyOrderId} is {orderStatus} ***')
-    if (orderStatus == "FILLED"):
-        print("*** Sell order will be send in a while ***")
-    else:
-        print("*** Order will be cancelled in a while ***")
-        cancelOrder('BTCUSDT',buyOrderId)
-        print('*** Order Cancelled ****')
-        quit()
-
-    # Wait for 30 seconds
-    time.sleep(30)
-
-    if (orderStatus == "FILLED"):
-        # Set Params to Sell
+    for i in range(5):
+        # Set Params to Buy
         params = {
         'symbol': 'BTCUSDT',
-        'side': 'SELL',
+        'side': 'BUY',
         'type': 'LIMIT',
         'timeInForce': 'GTC',
         'quantity': 0.003,
-        'price': round(float(umFuturesClient.ticker_price("BTCUSDT")["price"])-10) # Setting price as ticker price minus 10 so that buy order gets filled
+        'price': round(float(umFuturesClient.ticker_price("BTCUSDT")["price"]) + 10) # Setting price as ticker price plus 10 so that buy order gets filled
         }
 
-        # Execute Sell
-        sellOrderId = executeOrder(params)
-        print('\n\n*** SELL Order Sent ****')
+        # Execute Buy
+        buyOrderId = executeOrder(params)
+        print('\n\n*** BUY Order Sent ****')
+        print(f'Buy Order Id is {buyOrderId}')
+
+        # Wait for 10 seconds before next order
+        time.sleep(10)
 
         # Query Order to check if it was FILLED
-        time.sleep(10)
-        print("*** Checking Sell Order Status ***")
-        orderStatus = queryOrder("BTCUSDT",sellOrderId)
-        print(f'*** Order status for order id {sellOrderId} is {orderStatus} ***')
+        print("\n\n*** Checking Buy Order Status ***")
+        orderStatus = queryOrder("BTCUSDT",buyOrderId)
+        print(f'*** Order status for order id {buyOrderId} is {orderStatus} ***')
+        if (orderStatus == "FILLED"):
+            print("*** Sell order will be send in a while ***")
+        else:
+            print("*** Order will be cancelled in a while ***")
+            cancelOrder('BTCUSDT',buyOrderId)
+            print('*** Order Cancelled ****')
+
+
+
+        if (orderStatus == "FILLED"):
+            # Set Params to Sell
+            params = {
+            'symbol': 'BTCUSDT',
+            'side': 'SELL',
+            'type': 'LIMIT',
+            'timeInForce': 'GTC',
+            'quantity': 0.003,
+            'price': round(float(umFuturesClient.ticker_price("BTCUSDT")["price"])-10) # Setting price as ticker price minus 10 so that buy order gets filled
+            }
+
+            # Execute Sell
+            sellOrderId = executeOrder(params)
+            print('\n\n*** SELL Order Sent ****')
+
+            # Query Order to check if Sell Order was FILLED
+            print("*** Checking Sell Order Status ***")
+            sellOrderStatus = "NEW"
+            # Wait for Sell Order to fill
+            while (True):
+                sellOrderStatus = queryOrder("BTCUSDT",sellOrderId)
+                print(f'*** Order status for Sell order id {sellOrderId} is {sellOrderStatus} ***')
+                if (sellOrderStatus == "FILLED"):
+                    break
+            
+            # Wait for 10 seconds before next order
+            time.sleep(10)
 
